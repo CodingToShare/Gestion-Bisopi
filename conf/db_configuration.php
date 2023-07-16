956b4be8e111231806a3811b7ca18513ce81ddaf
Santiago Forero <santiago.forero@cmconsulting.com.co>
2023-07-16 16:22:51 -0500

    conexion MySQL Azure

--- conf/db_configuration.php
+++ conf/db_configuration.php
@@ -3,6 +3,6 @@
 	define( "DB_NAME" , 'bisionco_gestion_recurso_v2' );	
 	define( "DB_USERNAME" , 'bisionco_gestion_recurso' );
 	define( "DB_PASSWORD" , 'BS1234_gestion$' );
-	define( "DB_SERVER" , 'localhost' );
-	define( "DB_ENCODING", "latin1");
+	define( "DB_SERVER" , 'gestionbisopi.mysql.database.azure.com' );
+	define( "DB_ENCODING", "utf8mb4");
 ?>
